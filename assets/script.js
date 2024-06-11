441d8b862e1a88b1cd8241032735637f3b1b2f4e
wiiniss <152930468+wiiniss@users.noreply.github.com>
2024-06-11 15:33:13 -0300

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // if(html.classList.contains('light')) {
+  //  html.classList.remove('light')
+  //} else {
+  // html.classList.add('light')
+  //} 
+
+  //o toggle faz a mesma função q o codigo acima
+
+  html.classList.toggle('light')
+}
